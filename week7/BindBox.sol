e2f59181838f4bc286947d401b479fe75876ac03
JimLiu <jim.liu123@ehsn.com.tw>
2023-10-18 17:44:46 +0800

    d

--- week7/BindBox.sol
+++ week7/BindBox.sol
@@ -6,61 +6,47 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+/*totalSupply: 500
+mint(): 隨機 mint tokenId
+隨機的方式有以下選擇方式
+自己製作隨機 random，不限任何方法
+Chainlink VRF
+RANDAO
+Implement 盲盒機制
+*/
+/*
+0.insert the array[500] and value
+1.get random 
+2.find the array index value
+3.pop this value and remove this array.
+*/
 
-contract PuPuERC721 is ERC721 {
+
+contract BindBox is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    uint16 public constant totalSupply = 500;
+
+    string constant public blockUrl = "";
+
     string constant public baseUri = '{image:"https://imgur.com/IBDi02f"}';
 
-    // 1.create a ERC721 token with name and sybmol.
-    constructor() ERC721("Don't send NFT to me", "NONFT") {}
+    constructor() ERC721("bind box", "BBX") {
+    }
    
-    // 2.Have the mint function to mint a new token
-    function mint(address to) external {
+    function mint() external {
         uint256 newItemId = _tokenIds.current();
-        _safeMint(to, newItemId);
+        _safeMint(msg.sender, newItemId);
         _tokenIds.increment();
     }
 
-    // 3. The NFT Image is always the same,
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         _requireOwned(tokenId);
         return baseUri;
     }
-}
 
+    function getRandom() external returns(uint){
 
-contract ReturnPuPuERC721 is IERC721Receiver,PuPuERC721 {
-
-    address public who;
-    address public pupuAddr;
-
-    constructor(address checkERC721){
-        pupuAddr = checkERC721;
-    }
-
-    // 1.user will transfer the ERC721 to this contract.
-    function onERC721Received(
-        address operator,
-        address from,
-        uint256 tokenId,
-        bytes calldata data
-    ) external returns (bytes4){
-        who = msg.sender;
-        
-        // 2.check the sender(ERC721 contract) is the same as your ERC721.
-        // 3.if not, please transfer this token back to the sender.
-        if(msg.sender != pupuAddr){
-            ERC721 token = ERC721(operator);
-            token.transferFrom(address(this), from, tokenId);
-
-            // 4.and then,mint your token to this sender.
-            PuPuERC721 pu = PuPuERC721(pupuAddr);
-            pu.mint(from);
-        }
-
-        // 測試require就會roll back.
-        // require(msg.sender == pupuAddr,"Please don't transfer this NFT to me.");
     }
 }
